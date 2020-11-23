c02c98dd2d75d3fd58e1f891632201a1eff20d89
anac-jeronimo <anac_jeronimo@hotmail.com>
2020-11-23 18:23:53 +0000

    css missing

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import Header from "./Header";
+
+function Footer() {
+    return (
+        
+        <div className="footer">
+            <img src="./images/icon1.png"/>
+            <h3> </h3>
+            <p></p>
+            <img src="./images/icon2.png"/>
+            <h3></h3>
+            <p></p>
+            <img src="./images/icon3.png"/>
+            <h3></h3>
+            <p></p>
+            <img src="./images/icon4.png"/>
+            <h3></h3>
+            <p></p>
+        </div>
+       
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+  return (
+    <div className="header">
+       <img src="./images/ironhack-logo.svg" />
+                <img src="./images/menu-top.svg" />
+                <h1>"Say hello to ReactJS"</h1>
+                <p>You will learn how to use the most popular
+                    frontend library, and become a super Ninja developer. </p>
+                <button type="text">Awesome!</button>
+    </div>
+  );
+}
+
+export default Header;
