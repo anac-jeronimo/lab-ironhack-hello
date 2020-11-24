815e2c713c24d7c11d4a7652b0c748ab116d3728
anac-jeronimo <anac_jeronimo@hotmail.com>
2020-11-24 10:02:34 +0000

    some css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,37 @@ function Footer() {
     return (
         
         <div className="footer">
+            <div>
             <img src="./images/icon1.png"/>
-            <h3> </h3>
-            <p></p>
+            <h3>Declarative </h3>
+            <p>React makes it</p>
+            <p>painless to create</p>
+            <p>interactive UIs.</p>
+            </div>
+            <div>
             <img src="./images/icon2.png"/>
-            <h3></h3>
-            <p></p>
+            <h3>Components</h3>
+            <p>Build encapsulated</p>
+            <p>components that</p>
+            <p>manage their state.</p>
+            </div>
+            <div>
             <img src="./images/icon3.png"/>
-            <h3></h3>
-            <p></p>
+            <h3>Single-Way</h3>
+            <p>A set of immutable</p>
+            <p>values are passed to</p>
+            <p>the component's</p>
+            </div>
+            <div>
             <img src="./images/icon4.png"/>
-            <h3></h3>
-            <p></p>
+            <h3>JSX</h3>
+            <p>Statically-typed,</p>
+            <p>designed to run on</p>
+            <p>modern browsers.</p>
+            </div>
+            
         </div>
+       
        
     );
 }
